bcd538c3b5786ebf1709b6746cc2a79d9c28927c
swebb0708 <swebb792@gmail.com>
2018-11-15 13:29:41 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var newsContainer = document.querySelector("#div1");
+
+newsContainer.innerHTML +=
+`
+<div id="all">
+    <h1>Bloomberg<span>Opinion</span></h1>
+
+    <div class = "container">
+        <div class = "author">Tyler Cowen</div>
+        <div class = "title">The Carbon Tax is Dead. Long Live the Carbon Tax</div>
+    </div>
+    <div class = "container">
+        <div class = "author">Liam Denning</div>
+        <div class = "title">Tesla's New Chair Is Part of the Furniture</div>
+    </div>
+    <div class = "container">
+        <div class = "author">Leonid Bershidsky</div>
+        <div class = "title">Foldable Phones Are a Chance for a Tired Industry</div>
+    </div>
+    <div class = "container">
+        <div class = "author">Noah Smith</div>
+        <div class = "title">Democrats Should Lay Out a Grand Vision for America</div>
+    </div>
+    <div class = "container">
+        <div class = "author">The Editors</div>
+        <div class = "title">A Wave Election for Health-Care Reform</div>
+    </div>
+   <h5> READ MORE FROM OPINION \> </h5> 
+</div>
+
+<div id= "bottom"></div>
+<div id = "bttm"></div>s
+
+`
